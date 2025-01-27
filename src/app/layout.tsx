f4b9e3282c9552bc593cf8332c817fa76aca15d6
m-4verick <theodoreshin99@icloud.com>
2025-01-27 11:30:42 +0900

    :tada: init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../shared/styles/globals.css";
+import Header from "@/widgets/Header/Header";
 
 const pretendard = localFont({
     src: "../assets/fonts/PretendardVariable.woff2",
@@ -23,6 +24,7 @@ export default function RootLayout({
         <html lang="ko" className={`${pretendard.variable}`}>
         <body className="bg-background font-pretendard">
         <main>
+            <Header />
             {children}
         </main>
         </body>
