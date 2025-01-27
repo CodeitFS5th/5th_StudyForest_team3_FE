dcd9c7ee3fff6b2bbf0d2b2de38b35e7b3787bf9
m-4verick <theodoreshin99@icloud.com>
2025-01-27 10:42:54 +0900

    :tada: init

---
+++ src/views/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import MainImage from "@/views/MainPage/components/MainImage/MainImage";
+
+export default function MainPage() {
+    return (
+        <div>
+            <MainImage />
+        </div>
+    )
+}
