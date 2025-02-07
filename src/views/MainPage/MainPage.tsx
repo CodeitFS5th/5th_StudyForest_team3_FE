1c32192251675676994ef3c5fbe53676ac27b15d
김민희 <smnhree@gmail.com>
2025-02-07 12:17:46 +0900

    🧹 Remove unused imports from MainPage component

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import Input from "@/shared/components/inputField/Input";
-import { isValidEmail, isValidPassword } from "@/shared/utils/inputValidation";
-import Toast from "@/shared/components/toast/Toast";
-import { useToastControl } from "@/shared/hooks/useToastControl";
-import Textarea from "@/shared/components/inputField/Textarea";
 export default function MainPage() {
-  const { isToastVisible, showToast } = useToastControl();
   return (
     <div>
       <h1>메인페이지</h1>
