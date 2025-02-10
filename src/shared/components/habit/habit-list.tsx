c34f35bde1d679b265cff218e23f6c1f2e32e1bf
민지영 <kuck523@gmail.com>
2025-02-10 15:44:27 +0900

    ✨ Add habitModal
    ing

---
+++ src/shared/components/habit/habit-list.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import NewHabitItems from "./new-habit-items";
+import HabitItems from "./habit-items";
+import { useActionState, useEffect } from "react";
+import updateHabitAction from "./core/update-habit.action";
+
+interface IHabitList {
+  studyId: number;
+  onClose: () => void;
+}
+
+export default function HabitList({ studyId, onClose }: IHabitList) {
+  const [state, formAction, isPending] = useActionState(
+    updateHabitAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !isPending) {
+      onClose();
+    }
+  }, [state]);
+
+  return (
+    <form
+      action={formAction}
+      className="max-w-78 md:max-w-150 flex flex-col items-center"
+    >
+      <input name="studyId" id="studyId" value={studyId} hidden readOnly />
+
+      <h1 className="text-custom-color-black-400 text-2xl font-extrabold">
+        습관목록
+      </h1>
+      <div className="w-full flex flex-col gap-y-5 my-6 md:pl-24 md:pr-11">
+        <HabitItems studyId={studyId} />
+        <NewHabitItems studyId={studyId} />
+      </div>
+
+      <div className="flex gap-6">
+        <button
+          type="button"
+          onClick={() => onClose()}
+          className="cursor-pointer"
+        >
+          취소
+        </button>
+        <button
+          type="submit"
+          onClick={() => onClose()}
+          className="cursor-pointer"
+        >
+          수정완료
+        </button>
+      </div>
+    </form>
+  );
+}
