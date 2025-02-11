f073b192691f691819d8df810547014a1b0135e7
m-4verick <theodoreshin99@icloud.com>
2025-02-11 22:53:53 +0900

    :sparkles: 헤더 라우터 경로에 따라 버튼 없어지게

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,23 @@
+'use client'
 import Image from "next/image";
 import logo from "../../assets/images/logo.png";
 import { ButtonMakeStudy } from "@/components/button/ButtonRectangle";
+import {usePathname, useRouter} from "next/navigation";
 export default function Header() {
+    const pathname = usePathname()
+    const router = useRouter()
+
+    const handleLogoClick = () => {
+        router.push('/')
+    }
+
+    const hideButton = /^\/study\/[^/]+(\/habit|\/focus)?$/.test(pathname);
+
   return (
     <div className="w-full bg-custom-color-background">
       <div className="px-4 md:px-6 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
-        <Image src={logo} alt="logo image" className="w-[105px] md:w-[180px]" />
-        <ButtonMakeStudy />
+        <Image src={logo} alt="logo image" className="w-[105px] md:w-[180px] cursor-pointer" onClick={handleLogoClick}/>
+          {!hideButton && <ButtonMakeStudy/>}
       </div>
     </div>
   );
