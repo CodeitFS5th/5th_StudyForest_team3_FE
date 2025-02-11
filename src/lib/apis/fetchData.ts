7c34ae9ca1d84d4e926973f70459bdf12f27fa45
민지영 <kuck523@gmail.com>
2025-02-11 23:18:02 +0900

    ✨ Add fetchData
    GET method

--- src/lib/apis/fetchData.ts
+++ src/lib/apis/fetchData.ts
@@ -1,8 +1,8 @@
 type Cache =
-  | { cache: "no-store" }
-  | { cache: "no-cache" }
-  | { cache: "force-cache" }
-  | { next: { revalidate: number } };
+  | { cache: "no-store" } // 캐시 저장 않고 서버에서 불러옴
+  | { cache: "no-cache" } // 최신 캐시를 사용, 서버에 변경되면 변경된 것으로 업데이트
+  | { cache: "force-cache" } // 최신 캐시만 사용하고, 업데이트 안함
+  | { next: { revalidate: number } }; // 최신 캐시를 사용, 서버에 변경되면 변경된 것으로 업데이트, 근데 시간차를 둠
 
 const fetchData = async <T>(
   url: string,
